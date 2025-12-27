a76ab7da3c29889a0f0d488a36638315853d541d
Ganeshkandikonda <ganeshkandikonda.jfsjd@gmail.com>
2025-12-27 18:49:10 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/elite_3d_ai_portfolio/', // Your GitHub repo name
+    base: '/portfolio_updted/', // Your GitHub repo name
     build: {
         outDir: 'dist'
     }
